d4c803af723ac7cca956d8752225b7fc8f8fe353
Manuel Jesus Dominguez Gomez <45077443+MJDomGom@users.noreply.github.com>
2021-05-03 18:34:33 +0200

    Base de datos
    Se sube todo lo relacionado con la base de datos:
    -Fichero .xml con los modelos y una estructuracion de las vistas de la web
    -Fichero .sql con la base de datos sin datos integrados (se hara mas tarde)

---
+++ BD/48h.sql
@@ -0,0 +1,245 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-05-2021 a las 18:11:10
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `48h`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `nombre` varchar(100) NOT NULL,
+  `nomCorto` varchar(10) NOT NULL,
+  `idPartido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadisticas`
+--
+
+CREATE TABLE `estadisticas` (
+  `id` int(11) NOT NULL,
+  `golesTot` int(11) NOT NULL,
+  `tirosPuerta` int(11) NOT NULL,
+  `amarillas` int(11) NOT NULL,
+  `rojas` int(11) NOT NULL,
+  `azules` int(11) NOT NULL,
+  `ganadorEdc` varchar(500) NOT NULL,
+  `idEquipo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gestiona`
+--
+
+CREATE TABLE `gestiona` (
+  `nick` varchar(45) NOT NULL,
+  `idPartido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instalaciones`
+--
+
+CREATE TABLE `instalaciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` int(11) NOT NULL,
+  `capacidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `integrante`
+--
+
+CREATE TABLE `integrante` (
+  `dni` varchar(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `dorsal` int(11) NOT NULL,
+  `nombreEquipo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juegan`
+--
+
+CREATE TABLE `juegan` (
+  `idPartido` int(11) NOT NULL,
+  `idInstalacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partidos`
+--
+
+CREATE TABLE `partidos` (
+  `id` int(11) NOT NULL,
+  `resultado` varchar(10) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `nick` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `rol` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD PRIMARY KEY (`nombre`),
+  ADD KEY `idPartido` (`idPartido`);
+
+--
+-- Indices de la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEquipo` (`idEquipo`);
+
+--
+-- Indices de la tabla `gestiona`
+--
+ALTER TABLE `gestiona`
+  ADD KEY `nick` (`nick`,`idPartido`),
+  ADD KEY `idPartido` (`idPartido`);
+
+--
+-- Indices de la tabla `instalaciones`
+--
+ALTER TABLE `instalaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `integrante`
+--
+ALTER TABLE `integrante`
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `nombreEquipo` (`nombreEquipo`);
+
+--
+-- Indices de la tabla `juegan`
+--
+ALTER TABLE `juegan`
+  ADD KEY `idPartido` (`idPartido`,`idInstalacion`),
+  ADD KEY `idInstalacion` (`idInstalacion`);
+
+--
+-- Indices de la tabla `partidos`
+--
+ALTER TABLE `partidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`nick`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `instalaciones`
+--
+ALTER TABLE `instalaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `partidos`
+--
+ALTER TABLE `partidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `equipo`
+--
+ALTER TABLE `equipo`
+  ADD CONSTRAINT `equipo_ibfk_1` FOREIGN KEY (`idPartido`) REFERENCES `partidos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD CONSTRAINT `estadisticas_ibfk_1` FOREIGN KEY (`idEquipo`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `gestiona`
+--
+ALTER TABLE `gestiona`
+  ADD CONSTRAINT `gestiona_ibfk_1` FOREIGN KEY (`nick`) REFERENCES `usuario` (`nick`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `gestiona_ibfk_2` FOREIGN KEY (`idPartido`) REFERENCES `partidos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `integrante`
+--
+ALTER TABLE `integrante`
+  ADD CONSTRAINT `integrante_ibfk_1` FOREIGN KEY (`nombreEquipo`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `juegan`
+--
+ALTER TABLE `juegan`
+  ADD CONSTRAINT `juegan_ibfk_1` FOREIGN KEY (`idPartido`) REFERENCES `partidos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `juegan_ibfk_2` FOREIGN KEY (`idInstalacion`) REFERENCES `instalaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
